db8eb58bd9902d810e6c3a1f21aba942c67b2864
phamhongphuc <phamhongphuc.info@gmail.com>
2023-09-25 19:02:14 +0700

    chore(config/storybook): update config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,27 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          include: [path.resolve(__dirname, './src')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+          injectStoryParameters: false,
+        },
+        sourceLoaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
   ],
   framework: {
     name: '@storybook/react-webpack5',
